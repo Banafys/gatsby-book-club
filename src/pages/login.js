f26f9c41dc61a34b2e32788548e94f3b9a4fdd2d
banafys <22682230+Banafys@users.noreply.github.com>
2020-01-10 11:02:58 -0500

    Completed section 8: Handling Validation.

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,31 +2,46 @@ import React, { useState, useContext } from "react"
 import { FirebaseContext } from '../components/firebase';
 import { navigate } from "gatsby";
 
-import { Form, Input, Button } from '../components/common';
+import { Form, Input, Button, ErrorMessage } from '../components/common';
 
 const Login = () => {
     const [formValues, setFormValues] = useState({ email: '', password: '' });
     const { firebase } = useContext(FirebaseContext);
+    const [errorMessage, setErrorMessage] = useState('');
 
     function handleSubmit(e) {
         e.preventDefault();
-        firebase.login({ email: formValues.email, password: formValues.password }).then(() => navigate('/'));
+        firebase.login({ email: formValues.email, password: formValues.password })
+            .catch(error => {
+                setErrorMessage(error.message);
+            });
     }
 
     function handleInputChange(e) {
         e.persist();
+
         setFormValues(currentValues => ({
             ...currentValues,
             [e.target.name]: e.target.value
         }));
+
+        setErrorMessage('');
+
     }
 
     return (
         <section>
+
             <Form onSubmit={handleSubmit}>
-                <Input value={formValues.email} name="email" onChange={handleInputChange} type="email" placeholder="email" />
-                <Input value={formValues.password} name="password" onChange={handleInputChange} type="password" placeholder="password" />
+                <Input value={formValues.email} name="email" onChange={handleInputChange} type="email" placeholder="email" require />
+                <Input value={formValues.password} name="password" onChange={handleInputChange} type="password" placeholder="password" require />
                 <Button type="submit" block> Login </Button>
+
+                {!!errorMessage &&
+                    <ErrorMessage>
+                        {errorMessage}
+                    </ErrorMessage>
+                }
             </Form>
         </section>
     );
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,20 +1,27 @@
 import React, { useState, useContext } from 'react';
-import { Form, Input, Button } from '../components/common';
+import { Form, Input, Button, ErrorMessage } from '../components/common';
 import { FirebaseContext } from '../components/firebase';
 
 const Register = () => {
     const [formValues, setFormValues] = useState({ email: '', password: '', confirmPassword: '' });
     const { firebase } = useContext(FirebaseContext);
+    const [errorMessage, setErrorMessage] = useState('');
 
     function handleSubmit(e) {
         e.preventDefault();
         if (formValues.password === formValues.confirmPassword) {
             firebase.register({ email: formValues.email, password: formValues.password })
+                .catch(error => {
+                    setErrorMessage(error.message);
+                });
+        } else {
+            setErrorMessage('Password and Confirm Password fields must match.');
         }
     }
 
     function handleInputChange(e) {
         e.persist();
+        setErrorMessage('');
         setFormValues(currentValues => ({
             ...currentValues,
             [e.target.name]: e.target.value
@@ -24,8 +31,13 @@ const Register = () => {
         <section>
             <Form onSubmit={handleSubmit}>
                 <Input name="email" type="email" placeholder="email" onChange={handleInputChange} required />
-                <Input name="password" type="password" placeholder="password" minLength={3} onChange={handleInputChange} required />
-                <Input name="confirmPassword" type="password" placeholder="confirm password" minLength={3} onChange={handleInputChange} required />
+                <Input name="password" type="password" placeholder="password" minLength={6} onChange={handleInputChange} required />
+                <Input name="confirmPassword" type="password" placeholder="confirm password" minLength={6} onChange={handleInputChange} required />
+                {!!errorMessage &&
+                    <ErrorMessage>
+                        {errorMessage}
+                    </ErrorMessage>
+                }
                 <Button type="submit" block> Register </Button>
             </Form>
         </section>
